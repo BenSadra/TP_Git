5d70f8707e53912c1e6a1c233c308b68e0cafa41
kadi <kadi.amine@hotmail.fr>
2014-02-10 11:11:28 +0100

    effacer les annotations jaxb

--- src/org/kadi/model/Bateau.java
+++ src/org/kadi/model/Bateau.java
@@ -7,18 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlElements;
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name="bateau")
-@XmlAccessorType(XmlAccessType.FIELD)
-
-  
 
 public class Bateau implements Serializable {
 
@@ -30,23 +18,13 @@ public class Bateau implements Serializable {
 	private static final long serialVersionUID = -7473787322913073961L;
 
 
-	    @XmlAttribute
 	     private Long id;
-
-		 
-	    @XmlElement(name="nom")
 		private String nom;
-		 
 
-	    @XmlElementWrapper(name="equipage")
-	     @XmlElements(
-	        @XmlElement(name="marin",     type=Marin.class))
 	     private List<Marin> equipage=new ArrayList<Marin>();
 
 		
-		 
-
-		 public Bateau() {
+	 public Bateau() {
 			super();
 			// TODO Auto-generated constructor stub
 		}
